8b9491d7272eb9a37ba612ab4e3f12db48c3d063
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-15 16:55:12 +0800

    Update pull_k8s_cluster_img.sh

--- scripts/pull_k8s_cluster_img.sh
+++ scripts/pull_k8s_cluster_img.sh
@@ -4,8 +4,10 @@ ETCD_VERSION=3.2.18
 PAUSE_VERSION=3.1
 COREDNS_VERSION=1.1.3
 
+# DEFAULTBACKEND_VERSION=1.4
+DASHBOARD_VERSION=v1.10.0
 
-# 拉取镜像
+############ 拉取镜像
 docker pull anjia0532/google-containers.kube-apiserver-amd64:${K8S_VERSION}
 docker pull anjia0532/google-containers.kube-controller-manager-amd64:${K8S_VERSION}
 docker pull anjia0532/google-containers.kube-scheduler-amd64:${K8S_VERSION}
@@ -14,8 +16,11 @@ docker pull anjia0532/google-containers.pause:${PAUSE_VERSION}
 docker pull anjia0532/google-containers.etcd-amd64:${ETCD_VERSION}
 docker pull anjia0532/google-containers.coredns:${COREDNS_VERSION}
 
+#docker pull anjia0532/google-containers.defaultbackend:${DEFAULTBACKEND_VERSION}
+#docker pull anjia0532/google-containers.kubernetes-dashboard-amd64:${DASHBOARD_VERSION}
 
-## 修改tag
+
+############### 修改tag
 docker tag anjia0532/google-containers.kube-apiserver-amd64:${K8S_VERSION}  k8s.gcr.io/kube-apiserver-amd64:${K8S_VERSION}
 docker tag anjia0532/google-containers.kube-controller-manager-amd64:${K8S_VERSION}  k8s.gcr.io/kube-controller-manager-amd64:${K8S_VERSION}
 docker tag anjia0532/google-containers.kube-scheduler-amd64:${K8S_VERSION}  k8s.gcr.io/kube-scheduler-amd64:${K8S_VERSION}
@@ -24,8 +29,11 @@ docker tag anjia0532/google-containers.pause:${PAUSE_VERSION}   k8s.gcr.io/pause
 docker tag anjia0532/google-containers.etcd-amd64:${ETCD_VERSION}   k8s.gcr.io/etcd-amd64:${ETCD_VERSION}
 docker tag anjia0532/google-containers.coredns:${COREDNS_VERSION}   k8s.gcr.io/coredns:${COREDNS_VERSION}
 
+#docker tag anjia0532/google-containers.defaultbackend:${DEFAULTBACKEND_VERSION}  gcr.io/google_containers/defaultbackend:${DEFAULTBACKEND_VERSION}
+#docker tag anjia0532/google-containers.kubernetes-dashboard-amd64:${DASHBOARD_VERSION}  k8s.gcr.io/kubernetes-dashboard-amd64:${DASHBOARD_VERSION}
 
-## 删除镜像
+
+############### 删除镜像
 docker rmi anjia0532/google-containers.kube-apiserver-amd64:${K8S_VERSION}
 docker rmi anjia0532/google-containers.kube-controller-manager-amd64:${K8S_VERSION}
 docker rmi anjia0532/google-containers.kube-scheduler-amd64:${K8S_VERSION}
@@ -33,3 +41,6 @@ docker rmi anjia0532/google-containers.kube-proxy-amd64:${K8S_VERSION}
 docker rmi anjia0532/google-containers.pause:${PAUSE_VERSION}
 docker rmi anjia0532/google-containers.etcd-amd64:${ETCD_VERSION}
 docker rmi anjia0532/google-containers.coredns:${COREDNS_VERSION}
+
+# docker rmi anjia0532/google-containers.defaultbackend:${DEFAULTBACKEND_VERSION}
+# docker rmi anjia0532/google-containers.kubernetes-dashboard-amd64:${DASHBOARD_VERSION}
