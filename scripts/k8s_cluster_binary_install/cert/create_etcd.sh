c28d96e77f1ba7002d684e508cbada15099b410c
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 07:19:37 +0800

    Create create_etcd.sh

---
+++ scripts/k8s_cluster_binary_install/cert/create_etcd.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+# 创建证书签名请求
+mkdir /root/etcd
+cat > /root/etcd/etcd-csr.json <<EOF
+{
+  "CN": "etcd",
+  "hosts": [
+    "127.0.0.1",
+    "${ETCD1}",
+    "${ETCD2}",
+    "${ETCD3}"
+  ],
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "CN",
+      "ST": "BeiJing",
+      "L": "BeiJing",
+      "O": "k8s",
+      "OU": "4Paradigm"
+    }
+  ]
+}
+EOF
+
+# 生成证书
+cd /root/etcd
+cfssl gencert -ca=/etc/kubernetes/cert/ca.pem \
+    -ca-key=/etc/kubernetes/cert/ca-key.pem \
+    -config=/etc/kubernetes/cert/ca-config.json \
+    -profile=kubernetes etcd-csr.json | cfssljson -bare etcd
+    
+    
+# 拷贝证书
+mkdir -p /etc/etcd/cert 
+cp /root/etcd*.pem /etc/etcd/cert/ && chown -R k8s /etc/etcd/cert 
+    
+    
+    
+    
+    
+    
+    
