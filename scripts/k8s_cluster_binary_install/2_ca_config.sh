cb2ccdd395b8072939dad2cdada096c2658411e1
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-27 16:42:03 +0800

    Update 2_ca_config.sh

--- scripts/k8s_cluster_binary_install/2_ca_config.sh
+++ scripts/k8s_cluster_binary_install/2_ca_config.sh
@@ -62,13 +62,7 @@ cat > /root/ca/ca-csr.json <<EOF
 EOF
 
 # 生成CA证书和私钥
-cd /root/ca/ && ccfssl gencert -initca ca-csr.json | cfssljson -bare ca
+cd /root/ca/ && cfssl gencert -initca ca-csr.json | cfssljson -bare ca
 
 # 拷贝文件
 cp /root/ca/ca*.pem /root/ca/ca-config.json  /etc/kubernetes/cert && chown k8s /etc/kubernetes/cert
-
-
-
-
-
-
