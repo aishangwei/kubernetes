67e6afd6699fb04985314049c4706f7ea53cbdf1
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-27 20:22:32 +0800

    Create 05_flanneld_install.sh

---
+++ scripts/k8s_cluster_binary_install/05_flanneld_install.sh
@@ -0,0 +1,60 @@
+#!/usr/bin
+
+source ./00_cluster_env.sh
+
+mkdir flannel
+wget https://github.com/coreos/flannel/releases/download/v0.10.0/flannel-v0.10.0-linux-amd64.tar.gz
+tar -xzvf flannel-v0.10.0-linux-amd64.tar.gz -C flannel
+cp flannel/{flanneld,mk-docker-opts.sh}   /opt/k8s/bin/
+chmod +x /opt/k8s/bin/* && chown -R k8s  /opt/k8s/bin
+
+source /etc/profile.d/k8s.sh
+
+export IFACE=eth0
+cat > /etc/systemd/system/flanneld.service << EOF
+[Unit]
+Description=Flanneld overlay address etcd agent
+After=network.target
+After=network-online.target
+Wants=network-online.target
+After=etcd.service
+Before=docker.service
+
+[Service]
+Type=notify
+ExecStart=/opt/k8s/bin/flanneld \\
+  -etcd-cafile=/etc/kubernetes/cert/ca.pem \\
+  -etcd-certfile=/etc/flanneld/cert/flanneld.pem \\
+  -etcd-keyfile=/etc/flanneld/cert/flanneld-key.pem \\
+  -etcd-endpoints=${ETCD_ENDPOINTS} \\
+  -etcd-prefix=${FLANNEL_ETCD_PREFIX} \\
+  -iface=${IFACE}
+ExecStartPost=/opt/k8s/bin/mk-docker-opts.sh -k DOCKER_NETWORK_OPTIONS -d /run/flannel/docker
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+RequiredBy=docker.service
+EOF
+
+
+# etcdctl \
+  --endpoints=${ETCD_ENDPOINTS} \
+  --ca-file=/etc/kubernetes/cert/ca.pem \
+  --cert-file=/etc/flanneld/cert/flanneld.pem \
+  --key-file=/etc/flanneld/cert/flanneld-key.pem \
+  set ${FLANNEL_ETCD_PREFIX}/config '{"Network":"'${CLUSTER_CIDR}'", "SubnetLen": 24, "Backend": {"Type": "vxlan"}}'
+
+
+# 启动服务
+systemctl daemon-reload && systemctl enable flanneld && systemctl restart flanneld
+
+
+
+
+
+
+
+
+
+
