fb486b14320a1cd2b86d24fe110da628b6785b1b
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-27 16:18:50 +0800

    Update 1_sys_config.sh

--- scripts/k8s_cluster_binary_install/1_sys_config.sh
+++ scripts/k8s_cluster_binary_install/1_sys_config.sh
@@ -27,17 +27,25 @@ sudo service dnsmasq stop && systemctl disable dnsmasq
 # 加载内核模块
 sudo modprobe br_netfilter  && modprobe ip_vs
 
+# 设置系统参数
+cat > /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-iptables=1
+net.bridge.bridge-nf-call-ip6tables=1
+net.ipv4.ip_forward=1
+vm.swappiness=0
+vm.overcommit_memory=1
+vm.panic_on_oom=0
+fs.inotify.max_user_watches=89100
+EOF
+sudo sysctl -p /etc/sysctl.d/kubernetes.conf
+sudo mount -t cgroup -o cpu,cpuacct none /sys/fs/cgroup/cpu,cpuacct
+
 # 设置系统时区
 sudo timedatectl set-timezone Asia/Shanghai
 sudo timedatectl set-local-rtc 0
 sudo systemctl restart rsyslog
 sudo systemctl restart crond
 sudo mkdir -p /var/lib/etcd && chown -R k8s /etc/etcd/cert
-
-# 创建目录
-sudo mkdir -p /opt/k8s/bin && sudo chown -R k8s /opt/k8s
-sudo sudo mkdir -p /etc/kubernetes/cert && sudo chown -R k8s /etc/kubernetes
-sudo mkdir -p /etc/etcd/cert && sudo chown -R k8s /etc/etcd/cert
 
 # 添加 k8s和docker用户
 sudo useradd -m k8s
@@ -50,6 +58,12 @@ sudo sh -c 'echo 123456 | passwd k8s --stdin'
 sudo useradd -m docker
 sudo gpasswd -a k8s docker
 sudo mkdir -p /etc/docker
+
+# 创建目录
+sudo mkdir -p /opt/k8s/bin && sudo chown -R k8s /opt/k8s
+sudo sudo mkdir -p /etc/kubernetes/cert && sudo chown -R k8s /etc/kubernetes
+sudo mkdir -p /etc/etcd/cert && sudo chown -R k8s /etc/etcd/cert
+sudo mkdir -p /var/lib/etcd && chown -R k8s /var/lib/etcd
 
 
 # 设置镜像
