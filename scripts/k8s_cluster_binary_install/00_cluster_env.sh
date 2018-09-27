0312db4cec145d56f61c97abd5840293d707eecd
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-27 20:22:12 +0800

    Update 00_cluster_env.sh

--- scripts/k8s_cluster_binary_install/00_cluster_env.sh
+++ scripts/k8s_cluster_binary_install/00_cluster_env.sh
@@ -12,9 +12,15 @@ ETCD1N="c720141"
 ETCD2N="c720142"
 ETCD3N="c720143"
 
+# etcd 集群服务地址列表
+ETCD_ENDPOINTS="https://${ETCD1}:2379,https://${ETCD2}:2379,https://${ETCD3}:2379"
 
 
+# flanneld 网络配置前缀
+FLANNEL_ETCD_PREFIX="/kubernetes/network"
 
+# Pod 网段，建议 /16 段地址，部署前路由不可达，部署后集群内路由可达(flanneld 保证)
+CLUSTER_CIDR="172.30.0.0/16"
 
 
 
