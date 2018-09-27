4bf03ff5a8913a92f8cac4293c45f8c901d80176
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 07:15:54 +0800

    Create create_kubectl.sh

---
+++ scripts/k8s_cluster_binary_install/cert/create_kubectl.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+
+source ../00_cluster_env.sh
+
+
+# 创建 admin 证书和私钥
+mkdir -p /root/kubectl && cd /root/kubectl
+cat > /root/kubectl/admin-csr.json <<EOF
+{
+  "CN": "admin",
+  "hosts": [],
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "CN",
+      "ST": "BeiJing",
+      "L": "BeiJing",
+      "O": "system:masters",
+      "OU": "4Paradigm"
+    }
+  ]
+}
+EOF
+
+# 生成证书和私钥
+cd /root/kubectl
+cfssl gencert -ca=/etc/kubernetes/cert/ca.pem \
+  -ca-key=/etc/kubernetes/cert/ca-key.pem \
+  -config=/etc/kubernetes/cert/ca-config.json \
+  -profile=kubernetes admin-csr.json | cfssljson -bare admin
+
+
+# 创建 kubeconfig 文件
+
+# 设置集群参数
+kubectl config set-cluster kubernetes \
+  --certificate-authority=/etc/kubernetes/cert/ca.pem \
+  --embed-certs=true \
+  --server=${KUBE_APISERVER} \
+  --kubeconfig=kubectl.kubeconfig
+  
+# 设置客户端认证参数
+cd /root/kubectl
+kubectl config set-credentials admin \
+  --client-certificate=admin.pem \
+  --client-key=admin-key.pem \
+  --embed-certs=true \
+  --kubeconfig=kubectl.kubeconfig
+  
+# 设置上下文参数
+kubectl config set-context kubernetes \
+  --cluster=kubernetes \
+  --user=admin \
+  --kubeconfig=kubectl.kubeconfig
+  
+# 设置默认上下文
+kubectl config use-context kubernetes --kubeconfig=kubectl.kubeconfig
+  
+  
+# 拷贝 kubeconfig 配置文件
+mkdir -p ~/.kube
+cp /root/kubectl/kubectl.kubeconfig ~/.kube/
