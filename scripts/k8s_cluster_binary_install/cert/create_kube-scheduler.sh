48a3b721c2ce07fb33b951c12f4a8076be7e8ce2
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 08:08:54 +0800

    Create create_kube-scheduler.sh

---
+++ scripts/k8s_cluster_binary_install/cert/create_kube-scheduler.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+source ../00_cluster_env.sh
+
+# 创建证书签名请求
+mkdir /root/kube-scheduler
+cat > /root/kube-scheduler/kube-scheduler-csr.json <<EOF
+{
+    "CN": "system:kube-scheduler",
+    "hosts": [
+      "127.0.0.1",
+      "${MASTER1}",
+      "${MASTER2}",
+      "${MASTER3}"
+    ],
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+      {
+        "C": "CN",
+        "ST": "BeiJing",
+        "L": "BeiJing",
+        "O": "system:kube-scheduler",
+        "OU": "4Paradigm"
+      }
+    ]
+}
+EOF
+
+
+# 生成证书和私钥
+cd /root/kube-scheduler
+cfssl gencert -ca=/etc/kubernetes/cert/ca.pem \
+  -ca-key=/etc/kubernetes/cert/ca-key.pem \
+  -config=/etc/kubernetes/cert/ca-config.json \
+  -profile=kubernetes kube-scheduler-csr.json | cfssljson -bare kube-scheduler
+
+
+# 创建 kubeconfig 文件
+cd /root/kube-scheduler
+kubectl config set-cluster kubernetes \
+  --certificate-authority=/etc/kubernetes/cert/ca.pem \
+  --embed-certs=true \
+  --server=${KUBE_APISERVER} \
+  --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-credentials system:kube-scheduler \
+  --client-certificate=kube-scheduler.pem \
+  --client-key=kube-scheduler-key.pem \
+  --embed-certs=true \
+  --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-context system:kube-scheduler \
+  --cluster=kubernetes \
+  --user=system:kube-scheduler \
+  --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config use-context system:kube-scheduler --kubeconfig=kube-scheduler.kubeconfig
+
+# 拷贝配置 kubeconfig 到所有 master 节点
+cp /root/kube-scheduler/kube-scheduler.kubeconfig /etc/kubernetes/
+chown -R k8s  /etc/kubernetes
+
+
+
+
+
+
+
+
+
+
+
+
