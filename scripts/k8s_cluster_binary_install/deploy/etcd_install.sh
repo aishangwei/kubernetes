2da5754868253960324bae9084c454215e3954d2
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 10:31:44 +0800

    Create etcd_install.sh

---
+++ scripts/k8s_cluster_binary_install/deploy/etcd_install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+wget https://github.com/coreos/etcd/releases/download/v3.3.9/etcd-v3.3.9-linux-amd64.tar.gz
+tar -xvf etcd-v3.3.9-linux-amd64.tar.gz
+
+source ../00_cluster_env.sh
+cat > etcd.service.template <<EOF
+[Unit]
+Description=Etcd Server
+After=network.target
+After=network-online.target
+Wants=network-online.target
+Documentation=https://github.com/coreos
+
+[Service]
+User=k8s
+Type=notify
+WorkingDirectory=/var/lib/etcd/
+ExecStart=/opt/k8s/bin/etcd \\
+  --data-dir=/var/lib/etcd \\
+  --name=##NODE_NAME## \\
+  --cert-file=/etc/etcd/cert/etcd.pem \\
+  --key-file=/etc/etcd/cert/etcd-key.pem \\
+  --trusted-ca-file=/etc/kubernetes/cert/ca.pem \\
+  --peer-cert-file=/etc/etcd/cert/etcd.pem \\
+  --peer-key-file=/etc/etcd/cert/etcd-key.pem \\
+  --peer-trusted-ca-file=/etc/kubernetes/cert/ca.pem \\
+  --peer-client-cert-auth \\
+  --client-cert-auth \\
+  --listen-peer-urls=https://##NODE_IP##:2380 \\
+  --initial-advertise-peer-urls=https://##NODE_IP##:2380 \\
+  --listen-client-urls=https://##NODE_IP##:2379,http://127.0.0.1:2379 \\
+  --advertise-client-urls=https://##NODE_IP##:2379 \\
+  --initial-cluster-token=etcd-cluster-0 \\
+  --initial-cluster=${ETCD_NODES} \\
+  --initial-cluster-state=new
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65536
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
