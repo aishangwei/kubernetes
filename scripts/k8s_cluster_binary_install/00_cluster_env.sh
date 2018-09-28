fc646b7ecc881b44aebe6f537e14c475c87e6b70
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 19:01:59 +0800

    Update 00_cluster_env.sh

--- scripts/k8s_cluster_binary_install/00_cluster_env.sh
+++ scripts/k8s_cluster_binary_install/00_cluster_env.sh
@@ -23,10 +23,8 @@ ETCD1N="c720151"
 ETCD2N="c720152"
 ETCD3N="c720153"
 
-
-
-
-
+# 所有节点IP
+ALL_IPS=($MASTER1 $MASTER2 $MASTER3 $ETCD1 $ETCD2 $ETCD3 $NODE1 $NODE2 $NODE3)
 
 # Master 集群IP
 MASTER_IPS=($MASTER1 $MASTER2 $MASTER3)
@@ -43,16 +41,11 @@ ETCD_NODES="${ETCD1N}=https://${ETCD1}:2380,${ETCD2N}=https://${ETCD2}:2380,${ET
 # etcd 集群服务地址列表
 ETCD_ENDPOINTS="https://${ETCD1}:2379,https://${ETCD2}:2379,https://${ETCD3}:2379"
 
-
-# kube-apiserver 的 VIP（HA 组件 keepalived 发布的 IP）
-MASTER_VIP=192.168.20.151
-
 # kube-apiserver VIP 地址（HA 组件 haproxy 监听 8443 端口）
 KUBE_APISERVER="https://${MASTER_VIP}:8443"
 
 # kubernetes 服务 IP (一般是 SERVICE_CIDR 中第一个IP)
 CLUSTER_KUBERNETES_SVC_IP="10.254.0.1"
-
 
 # flanneld 网络配置前缀
 FLANNEL_ETCD_PREFIX="/kubernetes/network"
@@ -60,8 +53,11 @@ FLANNEL_ETCD_PREFIX="/kubernetes/network"
 # Pod 网段，建议 /16 段地址，部署前路由不可达，部署后集群内路由可达(flanneld 保证)
 CLUSTER_CIDR="172.30.0.0/16"
 
+# Haproxy 节点，VIP 所在的网络接口名称
+VIP_IF="eth0"
 
-
+# kube-apiserver 的 VIP（HA 组件 keepalived 发布的 IP）
+MASTER_VIP=192.168.20.150
 
 
 
