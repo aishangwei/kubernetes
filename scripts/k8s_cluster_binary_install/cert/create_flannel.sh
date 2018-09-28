85e515ac84fcec942d5d20da5ea7a6d11d303244
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 15:31:49 +0800

    Update create_flannel.sh

--- scripts/k8s_cluster_binary_install/cert/create_flannel.sh
+++ scripts/k8s_cluster_binary_install/cert/create_flannel.sh
@@ -3,8 +3,8 @@
 source ./00_cluster_env.sh
 
 # 创建证书签名请求
-mkdir /root/flanneld
-cat > /root/flanneld/flanneld-csr.json <<EOF
+mkdir ~/flanneld
+cat > ~/flanneld/flanneld-csr.json <<EOF
 {
   "CN": "flanneld",
   "hosts": [],
@@ -26,17 +26,13 @@ EOF
 
 
 # 生成证书和私钥
-cd /root/flanneld/
+cd ~/flanneld/
 cfssl gencert -ca=/etc/kubernetes/cert/ca.pem \
   -ca-key=/etc/kubernetes/cert/ca-key.pem \
   -config=/etc/kubernetes/cert/ca-config.json \
   -profile=kubernetes flanneld-csr.json | cfssljson -bare flanneld   
  
  
-# 拷贝证书和私钥到所有节点（master 和 worker）
-cd /root/flanneld
-cp flanneld*.pem  /etc/flanneld/cert/
-  
   
   
   
