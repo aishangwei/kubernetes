58577a9f89d615be88e2530323c64ea47d8e33c9
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 16:52:19 +0800

    Update cp_cert_service_to_flannel.sh

--- scripts/k8s_cluster_binary_install/cp/cp_cert_service_to_flannel.sh
+++ scripts/k8s_cluster_binary_install/cp/cp_cert_service_to_flannel.sh
@@ -25,18 +25,19 @@ for ip in ${ALL_IPS[@]}
     echo ">>> ${ip}"
     ssh root@${ip} "mkdir -p /etc/flanneld/cert && chown -R k8s /etc/flanneld"
     scp ~/flanneld/flanneld*.pem k8s@${ip}:/etc/flanneld/cert
+    scp ~/ca/ca.pem   k8s@${ip}:/etc/kubernetes/cert/
   done
 
-# 向 etcd 写入集群POD网段信息
-source ../00_cluster_env.sh
-etcdctlshell=etcdctl \
-  --endpoints=${ETCD_ENDPOINTS} \
-  --ca-file=/etc/kubernetes/cert/ca.pem \
-  --cert-file=/etc/flanneld/cert/flanneld.pem \
-  --key-file=/etc/flanneld/cert/flanneld-key.pem \
-  set ${FLANNEL_ETCD_PREFIX}/config '{"Network":"'${CLUSTER_CIDR}'", "SubnetLen": 24, "Backend": {"Type": "vxlan"}}'
 
-ssh root@${ETCD1} "${etcdctlshell}"
+# 向 etcd 写入集群POD网段信息,只需要执行一次，手工执行一次
+source ../00_cluster_env.sh
+#etcdctl \
+#  --endpoints=${ETCD_ENDPOINTS} \
+#  --ca-file=/etc/kubernetes/cert/ca.pem \
+#  --cert-file=/etc/flanneld/cert/flanneld.pem \
+#  --key-file=/etc/flanneld/cert/flanneld-key.pem \
+#  set ${FLANNEL_ETCD_PREFIX}/config '{"Network":"'${CLUSTER_CIDR}'", "SubnetLen": 24, "Backend": {"Type": "vxlan"}}'
+
 
 
 # 启动 flanneld 服务
