22cee8da4cff35fb2cd20398f11e1168cb5d4048
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 11:38:11 +0800

    Update kubectl_install.sh

--- scripts/k8s_cluster_binary_install/deploy/kubectl_install.sh
+++ scripts/k8s_cluster_binary_install/deploy/kubectl_install.sh
@@ -3,6 +3,10 @@
 wget https://dl.k8s.io/v1.11.3/kubernetes-client-linux-amd64.tar.gz
 tar -xzvf kubernetes-client-linux-amd64.tar.gz
 
+# 拷贝 kubectl到本机，用于生成配置文件
+cp kubernetes/client/bin/kubectl /opt/k8s/bin/
+
+
 
 
 
