4f69a95abcda83772ed7afaa6d0f08d18ce120a8
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 09:49:14 +0800

    Create cp_kubectl_to_usepoint.sh

---
+++ scripts/k8s_cluster_binary_install/cp/cp_kubectl_to_usepoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+
+#拷贝 kubectl 命令到使用节点
+source ../00_cluster_env.sh
+for ip in ${MASTER_IPS[@]}
+  do
+    echo ">>> ${ip}"
+    scp kubernetes/client/bin/kubectl k8s@${ip}:/opt/k8s/bin/
+    ssh k8s@${ip} "chmod +x /opt/k8s/bin/*"
+  done
+
+
+# 拷贝 kubeconfig 文件到 家目录
+source ../00_cluster_env.sh
+for ip in ${MASTER_IPS[@]}
+  do
+    echo ">>> ${ip}"
+    ssh k8s@${ip} "mkdir -p ~/.kube"
+    scp kubectl.kubeconfig k8s@${ip}:~/.kube/config
+    ssh root@${ip} "mkdir -p ~/.kube"
+    scp kubectl.kubeconfig root@${ip}:~/.kube/config
+  done
+
+
+
+
+
+
+
+
+
+
