cef82fa9a14ed9312c91586be3e7d471566c3705
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-28 10:20:32 +0800

    Create cp_service_cert_to_etcd.sh

---
+++ scripts/k8s_cluster_binary_install/cp/cp_service_cert_to_etcd.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# 拷贝 etcd 命令到 所有 etcd 节点
+source ../00_cluster_env.sh
+for ip in ${ETCD_IPS[@]}
+  do
+    echo ">>> ${ip}"
+    scp etcd-v3.3.9-linux-amd64/etcd* k8s@${node_ip}:/opt/k8s/bin
+    ssh k8s@${node_ip} "chmod +x /opt/k8s/bin/*"
+  done
+
+
+# 修改启动服务文件  systemd
+source ../00_cluster_env.sh
+for (( i=0; i < 3; i++ ))
+  do
+    sed -e "s/##ETCD_NAME##/${ETCD_NAMES[i]}/" -e "s/##ETCD_IP##/${ETCD_IPS[i]}/" ../deploy/etcd.service.template > ../deploy/etcd-${ETCD_IPS[i]}.service 
+  done
+
+
+# 拷贝启动服务文件  systemd
+source ../00_cluster_env.sh
+for ip in ${ETCD_IPS[@]}
+  do
+    echo ">>> ${ip}"
+    ssh root@${ip} "mkdir -p /var/lib/etcd && chown -R k8s /var/lib/etcd" 
+    scp etcd-${ip}.service root@${ip}:/etc/systemd/system/etcd.service
+  done
+
+
+# 拷贝证书和私钥
+source ../00_cluster_env.sh
+for ip in ${ETCD_IPS[@]}
+  do
+    echo ">>> ${ip}"
+    ssh root@${ip} "mkdir -p /etc/etcd/cert && chown -R k8s /etc/etcd/cert"
+    scp etcd*.pem k8s@${ip}:/etc/etcd/cert/
+  done
+
+
+# 启动 etcd 服务
+source ../00_cluster_env.sh
+for ip in ${ETCD_IPS[@]}
+  do
+    echo ">>> ${ip}"
+    ssh root@${node_ip} "systemctl daemon-reload && systemctl enable etcd && systemctl restart etcd &"
+  done
+
+
+
+
+
+
