ed1ac141efd67e1be3fe5244f7fcc016aac2b684
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-29 09:35:55 +0800

    Create master_slave_software_install.sh

---
+++ scripts/k8s_cluster_binary_install/deploy/master_slave_software_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+wget https://dl.k8s.io/v1.11.3/kubernetes-server-linux-amd64.tar.gz
+tar -xzvf kubernetes-server-linux-amd64.tar.gz
+cd kubernetes
+tar -xzvf  kubernetes-src.tar.gz
+
+source ../00_cluster_env.sh
+for ip in ${MASTER_IPS[@]}
+  do
+    echo ">>> ${ip}"
+    scp server/bin/* k8s@${ip}:/opt/k8s/bin/
+    ssh k8s@${node_ip} "chmod +x /opt/k8s/bin/*"
+  done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
