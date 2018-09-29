f3b9026cd77bd358c219c074a70cc368194bce0d
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-09-29 15:27:32 +0800

    Create kube-proxy_install.sh

---
+++ scripts/k8s_cluster_binary_install/deploy/kube-proxy_install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# 创建 kube-proxy 配置文件
+source ../00_cluster_env.sh
+cat >kube-proxy.config.yaml.template <<EOF
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+bindAddress: ##NODE_IP##
+clientConnection:
+  kubeconfig: /etc/kubernetes/kube-proxy.kubeconfig
+clusterCIDR: ${CLUSTER_CIDR}
+healthzBindAddress: ##NODE_IP##:10256
+hostnameOverride: ##NODE_NAME##
+kind: KubeProxyConfiguration
+metricsBindAddress: ##NODE_IP##:10249
+mode: "ipvs"
+EOF
+
+# 创建服务文件
+cat > kube-proxy.service <<EOF
+[Unit]
+Description=Kubernetes Kube-Proxy Server
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+After=network.target
+
+[Service]
+WorkingDirectory=/var/lib/kube-proxy
+ExecStart=/opt/k8s/bin/kube-proxy \\
+  --config=/etc/kubernetes/kube-proxy.config.yaml \\
+  --alsologtostderr=true \\
+  --logtostderr=false \\
+  --log-dir=/var/log/kubernetes \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
